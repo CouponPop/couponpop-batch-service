03e43968710c5bb84c9fddee8ffbd744dde1bdeb
younghunkimm <hunifive@gmail.com>
2025-10-25 22:07:25 +0900

    style: SchedulerConfig 작성 및 코드 정리

--- src/main/java/com/sparta/couponpopbatch/scheduler/MemberFcmTokenScheduler.java
+++ src/main/java/com/sparta/couponpopbatch/scheduler/MemberFcmTokenScheduler.java
@@ -2,11 +2,9 @@ package com.sparta.couponpopbatch.scheduler;
 
 import com.sparta.couponpopbatch.domain.member.service.MemberFcmTokenService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class MemberFcmTokenScheduler {
