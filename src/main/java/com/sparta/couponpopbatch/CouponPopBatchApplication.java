45dbc75908310b8a10bdbe2225ad773ec0dd01bd
younghunkimm <hunifive@gmail.com>
2025-10-25 14:20:36 +0900

    init

---
+++ src/main/java/com/sparta/couponpopbatch/CouponPopBatchApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.couponpopbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponPopBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponPopBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/couponpopbatch/CouponPopBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.couponpopbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponPopBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
