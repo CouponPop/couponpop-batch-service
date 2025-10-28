4f4b1639623f20666e800ecf08ecd510bd17eb3a
younghunkimm <hunifive@gmail.com>
2025-10-29 03:28:52 +0900

    fix: controller 예외 처리 추가

--- src/main/java/com/sparta/couponpopbatch/domain/coupon/controller/CouponUsageStatsJobController.java
+++ src/main/java/com/sparta/couponpopbatch/domain/coupon/controller/CouponUsageStatsJobController.java
@@ -6,6 +6,8 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,16 +24,21 @@ public class CouponUsageStatsJobController {
     private final JobRegistry jobRegistry;
 
     @PostMapping("/jobs/coupon-usage-stats")
-    public String launchCouponUsageStatsJob(
+    public ResponseEntity<String> launchCouponUsageStatsJob(
             @RequestParam LocalDate runDate
     ) throws Exception {
-        Job job = jobRegistry.getJob(COUPON_USAGE_STATS_JOB);
-        JobParameters jobParameters = new JobParametersBuilder()
-                .addLocalDate("runDate", runDate)
-                .toJobParameters();
+        try {
+            Job job = jobRegistry.getJob(COUPON_USAGE_STATS_JOB);
+            JobParameters jobParameters = new JobParametersBuilder()
+                    .addLocalDate("runDate", runDate)
+                    .toJobParameters();
 
-        jobLauncher.run(job, jobParameters);
-        return "Coupon Usage Stats Job has been launched.";
+            jobLauncher.run(job, jobParameters);
+
+            ResponseEntity.status(HttpStatus.OK).body("쿠폰 사용 통계 집계 배치 작업이 성공적으로 시작되었습니다.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("쿠폰 사용 통계 집계 배치 작업이 실패했습니다: " + e.getMessage());
+        }
     }
 
 }
