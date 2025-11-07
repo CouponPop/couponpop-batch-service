79114005195ee89e9cd1be9401a1715d303135f0
younghunkimm <hunifive@gmail.com>
2025-11-08 00:59:36 +0900

    chore: security module 버전 변경, config 파일에 주석 추가

--- src/main/java/com/couponpop/batchservice/common/config/SystemFeignConfig.java
+++ src/main/java/com/couponpop/batchservice/common/config/SystemFeignConfig.java
@@ -8,10 +8,20 @@ import org.springframework.context.annotation.Configuration;
 
 import static com.couponpop.security.constants.SecurityTemplates.BEARER_TOKEN_PREFIX;
 
+/**
+ * 시스템 내부 통신을 위한 Feign Client 설정입니다.
+ * 모든 요청에 시스템 토큰을 Authorization 헤더에 추가합니다.
+ */
 @Slf4j
 @Configuration
 public class SystemFeignConfig {
 
+    /**
+     * Feign 요청에 시스템 토큰을 추가하는 RequestInterceptor를 생성합니다.
+     *
+     * @param systemTokenProvider systemTokenProvider 시스템 토큰을 제공하는 프로바이더
+     * @return RequestInterceptor Bean
+     */
     @Bean
     public RequestInterceptor systemTokenRequestInterceptor(SystemTokenProvider systemTokenProvider) {
         return requestTemplate -> {
