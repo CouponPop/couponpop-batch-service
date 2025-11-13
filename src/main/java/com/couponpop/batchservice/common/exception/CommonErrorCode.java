a6baf9329ca6c4d4550ad767492fb983cce77bcf
younghunkimm <hunifive@gmail.com>
2025-11-13 14:17:41 +0900

    feat: batch api에 관리자 권한을 추가

--- src/main/java/com/couponpop/batchservice/common/exception/CommonErrorCode.java
+++ src/main/java/com/couponpop/batchservice/common/exception/CommonErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum CommonErrorCode implements ErrorCode {
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다. 관리자에게 문의해주세요."),
-    FIREBASE_INITIALIZATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "Firebase 초기화 중 오류가 발생했습니다.");
+    FIREBASE_INITIALIZATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "Firebase 초기화 중 오류가 발생했습니다."),
+    ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근 권한이 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
